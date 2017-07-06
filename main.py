3e48e7270b34a7598e86541966697e7c8967b4b5
Kamrin Peterson <kpeterson@hotmail.com>
2017-07-06 13:42:55 -0700

    Got welcome page to work and email messaging

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def ValidatePassword(password):
     elif len(password) < 3:
         return 'Password must be longer than 3 characters'
     elif len(password) > 20: 
-        return 'Password must be less than of equal to 20 characters'
+        return 'Password must be less than or equal to 20 characters'
     elif not str.isalpha(password):
         return "This is not a valid password, please use only letters"
     return ''
@@ -33,14 +33,21 @@ def ValidateVerify(password, verify):
     if verify == '' or password != verify:
         return 'Password and verify do not match'
     return ''
+def ValidateEmail(email):
+    if email == '':
+        return ''
+    elif len(email) < 3:
+        return 'Password must be longer than 3 characters'
+    elif len(email) > 20: 
+        return 'Password must be less than or equal to 20 characters'
+    elif not str.isalpha(password):
+        return "This is not a valid password, please use only letters"
+    return ''
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/", methods=['GET'])
-def welcome():
-    return render_template('welcome.html')
 
 @app.route("/", methods=['POST'])
 def postback():
@@ -55,40 +62,12 @@ def postback():
     
     if usernameMessage != '' or passwordMessage != '' or verifyMessage != '':
         return render_template('index.html',username=username, email=email, usernameMessage = usernameMessage, passwordMessage=passwordMessage, verifyMessage=verifyMessage)
-    return redirect('/welcome')
+    return render_template('welcome.html', username=username)
     #return render_template('index.html')
 #The user leaves any of the following fields empty: username, password, verify password.
 #The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
 #The user's password and password-confirmation do not match.
 #The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
 
-
-
-    #if ValidateUserName(username):
-        # the user tried to enter a user name that was not valid
-    #    error = "'{0}' is not a valid user name, please try again.".format(user_signup)
-        # redirect to homepage, and include error as a query parameter in the URL
-    #    return redirect("/?error=" + error)
-    #elif password not in Password():
-        # the user tried to enter a user name that was not valid
-    #    error = "'{0}' is not a valid user name, please try again.".format(user_signup)
-        # redirect to homepage, and include error as a query parameter in the URL
-    #    return redirect("/?error=" + error)
-    #elif verify != password:
-        # the user tried to enter a user name that was not valid
-    #    error = "'{0}' is not a valid user name, please try again.".format(user_signup)
-        # redirect to homepage, and include error as a query parameter in the URL
-    #    return redirect("/?error=" + error)
-    
-
-    # if we didn't redirect by now, then all is well
-    #return render_template('welcome.html', user_signup=user_signup)
-
-
-
-#@app.route("/")
-#def index():
-#    encoded_error = request.args.get("error")
-#    return render_template('index.html', usersignup=user_signup, error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
